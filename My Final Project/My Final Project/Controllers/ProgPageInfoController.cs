c6e14246667a3b22bd661da84001cf84d4553acd
Your Name <moonlight312004@gmail.com>
2023-06-22 21:31:04 +0100

    Prog. List Updates

---
+++ My Final Project/My Final Project/Controllers/ProgPageInfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BitLink.Controllers
+{
+    public class ProgPageInfoController : Controller
+    {
+        public IActionResult CSInfo()
+        {
+            return View();
+        }
+    }
+}
